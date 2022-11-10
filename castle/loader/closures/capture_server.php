dac4afd677ff15f5c1a488e71798a0c38b38bc61
yukihiro-asami <npbasami@gmail.com>
2022-11-10 13:13:58 +0900

    delete x_headers

--- castle/loader/closures/capture_server.php
+++ castle/loader/closures/capture_server.php
@@ -13,12 +13,6 @@ return function (array &$vals) : string
     $vals['request_method'] = $_SERVER['REQUEST_METHOD'];
     $vals['server_protocol'] = $_SERVER['SERVER_PROTOCOL'];
     $vals['https'] = $_SERVER['HTTPS'] ?? '';
-    $vals['http_x_forwarded_photo'] = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '';
-    $vals['http_x_forwarded_port'] = $_SERVER['HTTP_X_FORWARDED_PORT'] ?? '';
-    $vals['http_x_requested_with'] = $_SERVER['HTTP_X_REQUESTED_WITH'] ?? '';
-    $vals['http_x_cluster_client_ip'] = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'] ?? '';
-    $vals['http_x_forwarded_for'] = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? '';
-    $vals['http_x_method_override'] = $_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'] ?? '';
     $vals['method'] = $vals['security.allow_x_headers']
         ? _empty_next($vals['http_x_method_override'],  $vals['request_method'], 'GET')
         : _empty_next($vals['request_method'], 'GET');
